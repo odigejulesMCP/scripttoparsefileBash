b173d792b7041a058237b464321e32d895d14d5e
ojules <ojules30@outlook.com>
2023-02-02 10:14:40 -0500

    Redacted version

--- parseInBash.sh
+++ parseInBash.sh
@@ -1,7 +1,14 @@
-!/bin/bash
+#!/bin/bash
+
 LOGFILE="/home/script/Documents/messages"
+
 APPLICATION="puppet-agent"
+
 THRESHOLD=5
+
 grep $APPLICATION $LOGFILE | awk '{print $1, $2, $4, $5, $6}'
+
+. parseInBash.sh | wc -l
+
 #END
 
